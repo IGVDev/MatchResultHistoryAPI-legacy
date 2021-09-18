a067e723a06f8cdb14ddcaf9207bb94ec76430ce
IGVDev <guidoigv@gmail.com>
2021-09-17 22:34:20 -0300

    added database functionality

--- Controllers/matchload.js
+++ Controllers/matchload.js
@@ -1,21 +1,23 @@
 const handleMatchLoad = (req, res, db) => {
-   const { hteam, ateam, team1, team2, hscore, ascore, winner } = req.body;
-   if (!hteam || !ateam || !team1 || !team2 || !winner) return res.status(400).json('Missing data')
-      db('matches').insert({
-         hteam: hteam.toLowerCase(),
-         ateam: ateam.toLowerCase(),
-         team1: team1.toLowerCase(),
-         team2: team2.toLowerCase(),
-         hscore: hscore,
-         ascore: ascore,
-         winner: winner.toLowerCase(),
-         playedon: new Date()
-      })
-      .returning('*')
-      .then(match => res.json(match[0]))
-      .catch(err => res.status(200).json('Couldn\'t load match'))
-}
+  const { hteam, ateam, team1, team2, hscore, ascore, winner } = req.body;
+  if (!hteam || !ateam || !team1 || !team2 || !winner)
+    return res.status(400).json("Missing data");
+  db("matches")
+    .insert({
+      hteam: hteam.toLowerCase(),
+      ateam: ateam.toLowerCase(),
+      team1: team1.toLowerCase(),
+      team2: team2.toLowerCase(),
+      hscore: hscore,
+      ascore: ascore,
+      winner: winner.toLowerCase(),
+      playedon: new Date(),
+    })
+    .returning("*")
+    .then((match) => res.json(match[0]))
+    .catch((err) => res.status(200).json("Couldn't save match"));
+};
 
 module.exports = {
-   handleMatchLoad
-}
+  handleMatchLoad,
+};
